fbe7503933def2e6bcf34ddff7cb6b9a71e93e46
Casaubon <netzach.yoshida@gmail.com>
2022-08-18 22:01:52 +1200

    feat: 🎸 add SWR feature

--- apps/cf-pages/src/pages/index.tsx
+++ apps/cf-pages/src/pages/index.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import useSWR from 'swr'
+
+import fetcher from '../lib/fetch'
 
 
 type Props = {
   className: string
 }
 
-const Component: React.FC<Props> = ({className}) =>
-  (
+type SWRData = {
+  description: string
+}
+
+const Component: React.FC<Props> = ({className}) => {
+  const { data, error } = useSWR<SWRData>('https://api.github.com/repos/vercel/swr', fetcher)
+
+  if (error) return (<div>An error has occurred.</div>);
+  if (!data) return (<div>Loading...</div>);
+  return (
     <div className={className}>
       <h1>Enjoy!</h1>
       <p>hello cloudflare</p>
+      <p>{data.description}</p>
     </div>
   )
+}
 
 const StyledComponent = styled(Component)`
   display: grid;
