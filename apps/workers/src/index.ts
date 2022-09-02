2cdcc6eccad814e6532640302c03dc10ca2b2a14
Casaubon <netzach.yoshida@gmail.com>
2022-09-02 18:31:23 +1200

    feat: 🎸 add zod

--- apps/workers/src/index.ts
+++ apps/workers/src/index.ts
@@ -1,6 +1,22 @@
 import { Hono } from 'hono'
+import z from 'zod'
 
 const app = new Hono()
 app.get('/', (c) => c.text('Please test me!'))
+
+// form
+const formSchema = z.object({
+  body: z.object({
+    name: z.string().min(6)
+  })
+})
+
+app.post('/form', async (c) => {
+  const message = c.req.parseBody
+  const response = formSchema.parse({
+    body: message
+  })
+  return c.json(response)
+})
 
 export default app
